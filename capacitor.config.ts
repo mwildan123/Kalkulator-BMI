b084772cc6d231d063aa07089b6064060f20f2fc
mwildan123 <muhammadwildanfirdaus071@gmail.com>
2026-04-20 03:16:38 +0700

    fix: styling UI BMI calculator agar presisi

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'BmiCalculator',
+  appId: 'com.wildan.bmicalc',
+  appName: 'Kalkulator BMI',
   webDir: 'www'
 };
 
